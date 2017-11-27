5df4ae03a97fe29146e34781b03a181c26a69827
christopeher hanten <chanten@chanten-815.local>
2017-11-27 09:53:36 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("Up and running!");
+var cardOne = cards[1];
+var cardTwo = cards[3];
+var cardThree =cards[2]
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+cardsInPlay.push(cardOne);
+//cardsInPlay.push(cardTwo);
+cardsInPlay.push(cardThree)
+//console.log("User flipped queen");
+//console.log("User flipped king");
+
+if (cardsInPlay.length === 2) { 
+	 if(cardsInPlay[cardOne] === cardsInPlay[cardThree]){
+	 	alert("You found a match!");
+	} else 
+		alert("Sorry, try again.");
+}
+
+/*var answer;
+if (a == b) {
+  if (a == c) {
+    answer = "all are equal";
+  } else {
+    answer = "a and b are equal";
+  }*/
